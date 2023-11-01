e6470c1c81c574d0c9c7e93408dcbae4d83d91a2
ibrahim1996-serhane <ibrahim.serhane1996@gmail.com>
2023-11-01 15:56:47 +0200

    18th

--- myblog/src/App.js
+++ myblog/src/App.js
@@ -1,18 +1,27 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+
   useEffect(() => {
-    fetch('http://92.204.249.139:4000')
-      .then(res => res.json())
-      .then(data => setBlogs(data))
-  }, [])
+    // Make an Axios GET request
+    axios.get('http://92.204.249.139:4000')
+      .then(response => {
+        // Use response.data to access the JSON data
+        setBlogs(response.data);
+      })
+      .catch(error => {
+        // Handle any errors here
+        console.error("Error fetching data:", error);
+      });
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1> Docker 1.3 </h1>
+        <h1> Docker 1.4 </h1>
         {blogs && blogs.map(blog => (
           <div key={blog.id}>{blog.title}</div>
         ))}
